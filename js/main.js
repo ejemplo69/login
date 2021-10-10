e388601b66d76d57d018ce32074d7ee4900e05e6
unknown <miemail@dominio.com>
2021-10-10 12:35:09 -0500

    login_4

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-function login(){
+function login(){ //select en el php
 
     var user    = $.trim($("#username").val());
     var pass    = $.trim($("#password").val());
-
-    //alert(user+' '+pass);
 
     $.ajax({
         url: "php/consultas.php",
         type: "POST",
         dataType: "json",
-        data:  {usuario: user, contraseña: pass},
+        data:  {opc: '1', usuario: user, contraseña: pass},
         success: function(data){
-            alert('exito');
+            alert(data);
         },
         error: function(r){
             alert('algo esta mal');
@@ -23,4 +21,35 @@ function login(){
     // .fail(()=>{
     //     alert('algo esta mal');
     // })
+}
+
+
+function registrar(){ //el insert en el php
+    user  = $.trim($('#usuario').val());
+    email = $.trim($('#email').val());
+    cel   = $.trim($('#cel').val());
+    tel   = $.trim($('#tel').val());
+    pass  = $.trim($('#password').val());
+
+    if(user == "" || email =='' || pass ==""){
+        alert('un campo esta vacio');
+    }else{
+
+        $.ajax({
+            url:      'php/consultas.php',
+            type:     'POST',
+            dataTipe: 'json',
+            data:     {opc: '2',usuario: user, email: email, cel: cel, tel: tel, contraseña: pass},
+            success: function(data){
+                alert(data);
+                document.getElementById('contact').reset();
+            // window.location.replace('index.html');
+                window.location.href= 'index.html';
+
+            },
+            error: function(r){
+                alert('algo esta mal');
+            }
+        });
+}
 }
--- php/consultas.php
+++ php/consultas.php
@@ -1,8 +1,9 @@
 
 <?php
 
-    // $usuario     =   $_POST['usuario'];
-    // $contraseña  =   $_POST['contraseña'];
+    $usuario     =   $_POST['usuario'];
+    $contraseña  =   $_POST['contraseña'];
+    $opc         =   $_POST['opc'];
 
     // if($usuario == "jose"){
     //     echo true;
@@ -16,9 +17,44 @@
 
     $conexion = new mysqli($server, $user, $pass, $db);
 
-    if($conexion -> connect_error){
-        echo 'conexion fallo';
-    }else{
-        print('exito');
+    // if($conexion -> connect_error){
+    //     echo 'conexion fallo';
+    // }else{
+    //     print('exito');
+    // }
+
+    switch ($opc){
+        //login
+        case 1:
+            $sql = "SELECT * FROM usuario WHERE user = '$usuario' AND contraseña = '$contraseña'";//codigo sql
+
+            $consulta = mysqli_query($conexion, $sql) or die (mysqli_error());
+            if($fila= mysqli_fetch_array($consulta)){// mysqli_fetch_array contiene el numero de filas de una tabla
+                print json_encode($fila['email']); //pone en formato jason (json_encode)
+            }else{
+                print (false);
+            }
+            break;
+
+        //crear usuario    
+        case 2:
+            $email = $_POST['email'];
+            $cel   = $_POST['cel'];
+            $tel   = $_REQUEST['tel'];
+
+            // $sql = "INSERT INTO usuario (user, email, tel1, tel2, contraseña) VALUE ('$usuario','$email','$cel','$tel','$contraseña')";
+            $sql = "CALL `sp_insert_usuario`('$usuario', '$email', '$cel', '$tel', '$contraseña')";
+
+            $consulta = mysqli_query($conexion, $sql) or die (mysqli_error());
+            // echo true;
+            if($fila = mysqli_fetch_array($consulta)){
+                print json_encode($fila[0]);
+            }
+            break;
+
+        //update    
     }
+    
+        
+
 ?>
